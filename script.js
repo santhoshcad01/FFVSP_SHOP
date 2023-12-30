7cc74b68f96f82290f149abb02ca79dc0be7a4f1
santhoshcad01@gmail.com <santhoshcad01@brightskills.com>
2023-12-30 17:59:36 +0530

    all files

---
+++ script.js
@@ -0,0 +1,16 @@
+function myprogram(){
+    var a=12;
+    var b=25;
+    var c=30;
+
+    console.log(a);
+    
+
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a%b);
+    console.log(a+b+c);
+
+}
+myprogram();
